2ab0b9c11572cc301abbf96890826b3ec3e062b2
Mathan Thava <mathan.thavachelvam@hmcts.net>
2018-07-05 09:46:46 +0100

    infrastructure fixes

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -12,7 +12,7 @@ locals {
   aseName = "${data.terraform_remote_state.core_apps_compute.ase_name[0]}"
   local_env = "${(var.env == "preview" || var.env == "spreview") ? (var.env == "preview" ) ? "aat" : "saat" : var.env}"
 
-  fee_api_url = "${var.fee_api_url == "" ? "http://fee-api-${local.local_env}.service.core-compute-${local.local_env}.internal" : var.fee_api_url}"
+//  fee_api_url = "${var.fee_api_url == "" ? "http://fee-api-${local.local_env}.service.core-compute-${local.local_env}.internal" : var.fee_api_url}"
 }
 
 
@@ -30,7 +30,7 @@ module "div-fees-and-payment-service" {
     REFORM_TEAM = "${var.product}"
     REFORM_SERVICE_NAME = "${var.reform_service_name}"
     REFORM_ENVIRONMENT = "${var.env}"
-    FEE_API_URL = "${local.fee_api_url}${var.fee_api_endpoint}"
+    FEE_API_URL = "${var.fee_api_url}${var.fee_api_endpoint}"
   }
 }
 
