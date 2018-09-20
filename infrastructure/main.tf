626c0e7b69e867fba3cd861b1a71162f84e26254
Tony Chow <36265570+tchow8@users.noreply.github.com>
2018-09-20 09:30:31 +0100

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -5,7 +5,7 @@ locals {
   fee_api_url = "${var.fee_api_url == "" ? "http://fees-register-api-${local.local_env}.service.core-compute-${local.local_env}.internal" : var.fee_api_url}"
 
   asp_name = "${var.env == "prod" ? "div-fps-prod" : "${var.product}-${var.env}"}"
-  asp_rg = "${var.env == "prod" ? "div-fps-prod" : "${var.product}-shared-infrastructure-${var.env}"}"
+  asp_rg = "${var.env == "prod" ? "div-fps-prod" : "${var.product}-${var.env}"}"
 }
 
 
