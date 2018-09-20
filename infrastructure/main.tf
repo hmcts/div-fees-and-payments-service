d2b423be89efd087ad22b53c46c593055a27f82b
Tony Chow <36265570+tchow8@users.noreply.github.com>
2018-09-20 04:08:09 +0100

    Use shared infrastructure

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -3,18 +3,23 @@ locals {
   local_env = "${(var.env == "preview" || var.env == "spreview") ? (var.env == "preview" ) ? "aat" : "saat" : var.env}"
 
   fee_api_url = "${var.fee_api_url == "" ? "http://fees-register-api-${local.local_env}.service.core-compute-${local.local_env}.internal" : var.fee_api_url}"
+
+  asp_name = "${var.env == "prod" ? "div-fps-prod" : "${var.product}-${var.env}"}"
+  asp_rg = "${var.env == "prod" ? "div-fps-prod" : "${var.product}-shared-infrastructure-${var.env}"}"
 }
 
 
 module "div-fees-and-payment-service" {
-  source = "git@github.com:hmcts/moj-module-webapp.git"
-  product = "${var.product}-${var.reform_service_name}"
-  location = "${var.location}"
-  env = "${var.env}"
-  ilbIp = "${var.ilbIp}"
-  is_frontend = false
-  subscription = "${var.subscription}"
-  common_tags  = "${var.common_tags}"
+  source                          = "git@github.com:hmcts/moj-module-webapp.git"
+  product                         = "${var.product}-${var.reform_service_name}"
+  location                        = "${var.location}"
+  env                             = "${var.env}"
+  ilbIp                           = "${var.ilbIp}"
+  is_frontend                     = false
+  subscription                    = "${var.subscription}"
+  common_tags                     = "${var.common_tags}"
+  asp_name                        = "${local.asp_name}"
+  asp_rg                          = "${local.asp_rg}"
 
   app_settings = {
     //    logging vars
