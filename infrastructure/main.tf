2990bcbaec8e22d96968107ee24c5eafee10183c
Tony Chow <36265570+tchow8@users.noreply.github.com>
2019-01-14 16:41:49 +0000

    Move to shared ASP 1

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -4,8 +4,8 @@ locals {
 
   fee_api_url = "${var.fee_api_url == "" ? "http://fees-register-api-${local.local_env}.service.core-compute-${local.local_env}.internal" : var.fee_api_url}"
 
-  asp_name = "${var.env == "prod" ? "div-fps-prod" : "${var.raw_product}-${var.env}"}"
-  asp_rg = "${var.env == "prod" ? "div-fps-prod" : "${var.raw_product}-${var.env}"}"
+  asp_name = "${var.env == "prod" ? "div-fps-prod" : "${var.raw_product}-1-${var.env}"}"
+  asp_rg = "${var.env == "prod" ? "div-fps-prod" : "${var.raw_product}-1-${var.env}"}"
 }
 
 
