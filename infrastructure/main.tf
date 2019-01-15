14ab20e56e1f0a20e23e34fd05082184c8af25c9
Tony Chow <36265570+tchow8@users.noreply.github.com>
2019-01-15 09:55:05 +0000

    Revert to normal ASP and use larger instance size

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -4,8 +4,8 @@ locals {
 
   fee_api_url = "${var.fee_api_url == "" ? "http://fees-register-api-${local.local_env}.service.core-compute-${local.local_env}.internal" : var.fee_api_url}"
 
-  asp_name = "${var.env == "prod" ? "div-fps-prod" : "${var.raw_product}-1-${var.env}"}"
-  asp_rg = "${var.env == "prod" ? "div-fps-prod" : "${var.raw_product}-1-${var.env}"}"
+  asp_name = "${var.env == "prod" ? "div-fps-prod" : "${var.raw_product}-${var.env}"}"
+  asp_rg = "${var.env == "prod" ? "div-fps-prod" : "${var.raw_product}-${var.env}"}"
 }
 
 
@@ -20,6 +20,7 @@ module "div-fees-and-payment-service" {
   common_tags                     = "${var.common_tags}"
   asp_name                        = "${local.asp_name}"
   asp_rg                          = "${local.asp_rg}"
+  instance_size                   = "I3"
 
   app_settings = {
     //    logging vars
