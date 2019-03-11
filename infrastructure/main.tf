0c42628955ee19bcfd76e24945d424ee8f83d6ba
tony.chow <tony.chow@hmcts.net>
2019-03-11 11:14:01 +0000

    Remove state and add data provider
    Remove provider and use shared infra version

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,8 +1,8 @@
 locals {
-  aseName = "${data.terraform_remote_state.core_apps_compute.ase_name[0]}"
+  aseName = "core-compute-${var.env}"
   local_env = "${(var.env == "preview" || var.env == "spreview") ? (var.env == "preview" ) ? "aat" : "saat" : var.env}"
 
-  fee_api_url = "${var.fee_api_url == "" ? "http://fees-register-api-${local.local_env}.service.core-compute-${local.local_env}.internal" : var.fee_api_url}"
+  fee_api_url = "${var.fee_api_url == "" ? "http://fees-register-api-${local.local_env}.service.${local.aseName}.internal" : var.fee_api_url}"
 
   asp_name = "${var.env == "prod" ? "div-fps-prod" : "${var.raw_product}-${var.env}"}"
   asp_rg = "${var.env == "prod" ? "div-fps-prod" : "${var.raw_product}-${var.env}"}"
