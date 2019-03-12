b553c062510cc5b71ca4852e69b2c1d1fb67d23f
tony.chow <tony.chow@hmcts.net>
2019-03-12 00:31:55 +0000

    Fixed url in main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -2,7 +2,7 @@ locals {
   aseName = "core-compute-${var.env}"
   local_env = "${(var.env == "preview" || var.env == "spreview") ? (var.env == "preview" ) ? "aat" : "saat" : var.env}"
 
-  fee_api_url = "${var.fee_api_url == "" ? "http://fees-register-api-${local.local_env}.service.${local.aseName}.internal" : var.fee_api_url}"
+  fee_api_url = "${var.fee_api_url == "" ? "http://fees-register-api-${local.local_env}.service.core-compute-${local.local_env}.internal" : var.fee_api_url}"
 
   asp_name = "${var.env == "prod" ? "div-fps-prod" : "${var.raw_product}-${var.env}"}"
   asp_rg = "${var.env == "prod" ? "div-fps-prod" : "${var.raw_product}-${var.env}"}"
