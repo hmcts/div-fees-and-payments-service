b390508b61da9ab0e6c5c1a992fb1c841135774d
Tony Chow <tony.chow@hmcts.net>
2019-04-04 15:42:25 +0100

    Use Shared App Insights

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -72,3 +72,8 @@ variable "vault_env" {}
 variable "common_tags" {
     type = "map"
 }
+
+variable "appinsights_instrumentation_key" {
+  description = "Instrumentation key of the App Insights instance this webapp should use. Module will create own App Insights resource if this is not provided"
+  default = ""
+}
