b2b047889f26fe2a990516508fde4bd0089e3ead
Thomas Seelig <20642658+TSeelig@users.noreply.github.com>
2020-10-07 10:31:44 +0100

    upgrade terraform (#170)
    * upgrade terraform
    
    * supress CVE

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,11 +1,15 @@
+provider "azurerm" {
+  features {}
+}
+
 locals {
   aseName = "core-compute-${var.env}"
-  local_env = "${(var.env == "preview" || var.env == "spreview") ? (var.env == "preview" ) ? "aat" : "saat" : var.env}"
+  local_env = (var.env == "preview" || var.env == "spreview") ? (var.env == "preview" ) ? "aat" : "saat" : var.env
 
-  fee_api_url = "${var.fee_api_url == "" ? "http://fees-register-api-${local.local_env}.service.core-compute-${local.local_env}.internal" : var.fee_api_url}"
+  fee_api_url = var.fee_api_url == "" ? "http://fees-register-api-${local.local_env}.service.core-compute-${local.local_env}.internal" : var.fee_api_url
 
-  asp_name = "${var.env == "prod" ? "div-fps-prod" : "${var.raw_product}-${var.env}"}"
-  asp_rg = "${var.env == "prod" ? "div-fps-prod" : "${var.raw_product}-${var.env}"}"
+  asp_name = var.env == "prod" ? "div-fps-prod" : "${var.raw_product}-${var.env}"
+  asp_rg = var.env == "prod" ? "div-fps-prod" : "${var.raw_product}-${var.env}"
 }
 
 
