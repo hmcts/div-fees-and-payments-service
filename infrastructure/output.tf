e1e5bda4f23dace179a9650dd8d8248692f512dd
TSeelig <thomas.seelig@HMCTS.NET>
2021-03-29 13:39:55 +0100

    update terraform

--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -1,3 +1,11 @@
 output "test_environment" {
   value = local.local_env
 }
+
+output "vaultUri" {
+  value = data.azurerm_key_vault.div_key_vault.vault_uri
+}
+
+output "vaultName" {
+  value = local.vaultName
+}
