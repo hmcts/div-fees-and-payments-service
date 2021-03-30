cacac8f4bd0dc7f1882f56f56ce089e784afaa76
Thomas Seelig <20642658+TSeelig@users.noreply.github.com>
2021-03-30 12:25:03 +0100

    Fix azure logging (#222)
    * fix logs not showing up in Azure
    
    * fix logs not showing up in Azure
    
    * update terraform
    
    * update terraform vars
    
    * add identity name

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -4,12 +4,18 @@ provider "azurerm" {
 
 locals {
   aseName = "core-compute-${var.env}"
+  vaultName = "${var.product}-${var.env}"
   local_env = (var.env == "preview" || var.env == "spreview") ? (var.env == "preview" ) ? "aat" : "saat" : var.env
 
   fee_api_url = var.fee_api_url == "" ? "http://fees-register-api-${local.local_env}.service.core-compute-${local.local_env}.internal" : var.fee_api_url
 
   asp_name = var.env == "prod" ? "div-fps-prod" : "${var.raw_product}-${var.env}"
   asp_rg = var.env == "prod" ? "div-fps-prod" : "${var.raw_product}-${var.env}"
+}
+
+data "azurerm_key_vault" "div_key_vault" {
+  name                = local.vaultName
+  resource_group_name = local.vaultName
 }
 
 
