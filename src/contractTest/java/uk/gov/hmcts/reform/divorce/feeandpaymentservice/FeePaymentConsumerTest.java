38506f6a522ad1bb89e5e439ca2a5a5dfa71c1c0
Ruban <ruban72@hotmail.com>
2021-10-01 14:47:52 +0100

    DFR-89: Initial commit

--- src/contractTest/java/uk/gov/hmcts/reform/divorce/feeandpaymentservice/FeePaymentConsumerTest.java
+++ src/contractTest/java/uk/gov/hmcts/reform/divorce/feeandpaymentservice/FeePaymentConsumerTest.java
@@ -14,7 +14,10 @@ import org.json.JSONException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import uk.gov.hmcts.reform.divorce.feepayment.FeesPaymentServiceApplication;
 
 import java.io.IOException;
 
@@ -22,190 +25,213 @@ import java.io.IOException;
 @ExtendWith(SpringExtension.class)
 @PactTestFor(providerName = "feeRegister_lookUp", port = "4009")
 @PactFolder("build/pacts")
-
+@SpringBootTest(classes = FeesPaymentServiceApplication.class)
 public class FeePaymentConsumerTest {
 
+    @Value("${feature-toggle.toggle.fee-pay-keywords}")
+    private Boolean featureToggleKeywords;
+
     private static final String BASEURI = "/fees-register/fees/lookup";
-    private static final String PETITION_ISSUE_FEE = "channel=default&event=issue&jurisdiction1=family&"
-            + "jurisdiction2=family%20court&service=divorce";
-    private static final String AMEND_FEE = "channel=default&event=issue&jurisdiction1=family&jurisdiction2=family%20court&service=other&keyword=ABC";
-    private static final String DEFENDENT_FEE = "channel=default&event=issue&jurisdiction1=family&"
-            + "jurisdiction2=family%20court&service=other&keyword=PQR";
-    private static final String GENRALAPPLICATION_FEE = "channel=default&event=general%20application&jurisdiction1=family&"
-            + "jurisdiction2=family%20court&service=other";
-    private static final String ENFORCEMENT_FEE = "channel=default&event=enforcement&jurisdiction1=family&"
-            + "jurisdiction2=family%20court&keyword=HIJ&service=other";
-    private static final String FINANCIALORDER_FEE = "channel=default&event=miscellaneous&jurisdiction1=family&"
-            + "jurisdiction2=family%20court&keyword=financial-order&service=other";
-    private static final String APPLICATION_NO_NOTICE = "channel=default&event=general%20application&jurisdiction1=family&"
-            + "jurisdiction2=family%20court&keyword=GeneralAppWithoutNotice&service=other";
+    private String petitionIssueFee;
+    private String amendFee;
+    private String defendantFee;
+    private String generalApplicationFee;
+    private String enforcementFee;
+    private String financialOrderFee;
+    private String applicationNoNotice;
 
     @BeforeEach
     public void setUpEachTest() throws InterruptedException {
         Thread.sleep(2000);
+
+        petitionIssueFee = "channel=default&event=issue&jurisdiction1=family&"
+            + "jurisdiction2=family%20court&service=divorce" + (featureToggleKeywords ? "&keyword=DivorceCivPart" : "");
+
+        amendFee = "channel=default&event=issue&jurisdiction1=family&"
+            + "jurisdiction2=family%20court&service=other"
+            + (featureToggleKeywords ? "&keyword=DivorceAmendPetition" : "&keyword=ABC");
+
+        defendantFee = "channel=default&event=issue&jurisdiction1=family&"
+            + "jurisdiction2=family%20court&service=other"
+            + (featureToggleKeywords ? "&keyword=AppnPrivateOther" : "&keyword=PQR");
+
+        generalApplicationFee = "channel=default&event=general%20application&jurisdiction1=family&"
+            + "jurisdiction2=family%20court&service=other"
+            + (featureToggleKeywords ? "&keyword=GAContestedOrder" : "");
+
+        enforcementFee = "channel=default&event=enforcement&jurisdiction1=family&"
+            + "jurisdiction2=family%20court&keyword=BailiffServeDoc&service=other"
+            + (featureToggleKeywords ? "&keyword=BailiffServeDoc" : "&keyword=HIJ");
+
+        financialOrderFee = "channel=default&event=miscellaneous&jurisdiction1=family&"
+            + "jurisdiction2=family%20court&service=other"
+            + (featureToggleKeywords ? "&keyword=FinancialOrderOnNotice" : "&keyword=financial-order");
+
+        applicationNoNotice = "channel=default&event=general%20application&jurisdiction1=family&"
+            + "jurisdiction2=family%20court&keyword=GeneralAppWithoutNotice&service=other";
     }
 
     @Pact(consumer = "divorce_feeAndPaymentService_feeRegister")
     public RequestResponsePact pettitionIssueFee(PactDslWithProvider builder) {
 
         return builder.given("service is registered in Fee registry")
-                .uponReceiving("request for petition issue fee")
-                .path(BASEURI)
-                .encodedQuery(PETITION_ISSUE_FEE)
-                .method("GET")
-                .willRespondWith()
-                .status(200)
-                .body(getFeeResponseBody("FEE0002", 550.00, 4,
-                        "Filing an application for a divorce, nullity or civil partnership dissolution – fees order 1.2."))
-                .toPact();
+            .uponReceiving("request for petition issue fee")
+            .path(BASEURI)
+            .encodedQuery(petitionIssueFee)
+            .method("GET")
+            .willRespondWith()
+            .status(200)
+            .body(getFeeResponseBody("FEE0002", 550.00, 4,
+                "Filing an application for a divorce, nullity or civil partnership dissolution – fees order 1.2."))
+            .toPact();
     }
 
     @Test
     @PactTestFor(pactMethod = "pettitionIssueFee")
     public void verifyPetitionIssueFeePact(MockServer mockServer) throws IOException, JSONException {
-        HttpResponse httpResponse = Request.Get(mockServer.getUrl() + BASEURI + "?" + PETITION_ISSUE_FEE)
-                .execute().returnResponse();
+        HttpResponse httpResponse = Request.Get(mockServer.getUrl() + BASEURI + "?" + petitionIssueFee)
+            .execute().returnResponse();
     }
 
     @Pact(consumer = "divorce_feeAndPaymentService_feeRegister")
     public RequestResponsePact getAmendFee(PactDslWithProvider builder) {
 
         return builder.given("service is registered in Fee registry")
-                .uponReceiving("received request for Amend issue fee")
-                .path(BASEURI)
-                .encodedQuery(AMEND_FEE)
-                .method("GET")
-                .willRespondWith()
-                .status(200)
-                .body(getFeeResponseBody("FEE0233", 95.00, 1,
-                        "Amendment of application for matrimonial/civil partnership orde"))
-                .toPact();
+            .uponReceiving("received request for Amend issue fee")
+            .path(BASEURI)
+            .encodedQuery(amendFee)
+            .method("GET")
+            .willRespondWith()
+            .status(200)
+            .body(getFeeResponseBody("FEE0233", 95.00, 1,
+                "Amendment of application for matrimonial/civil partnership orde"))
+            .toPact();
     }
 
     @Test
     @PactTestFor(pactMethod = "getAmendFee")
     public void verifyAmendFeePact(MockServer mockServer) throws IOException {
-        HttpResponse httpResponse = Request.Get(mockServer.getUrl() + BASEURI + "?" + AMEND_FEE)
-                .execute().returnResponse();
+        HttpResponse httpResponse = Request.Get(mockServer.getUrl() + BASEURI + "?" + amendFee)
+            .execute().returnResponse();
     }
 
     @Pact(consumer = "divorce_feeAndPaymentService_feeRegister")
     public RequestResponsePact getDefendantFee(PactDslWithProvider builder) {
 
         return builder.given("service is registered in Fee registry")
-                .uponReceiving("received request for Defendant issue fee")
-                .path(BASEURI)
-                .encodedQuery(DEFENDENT_FEE)
-                .method("GET")
-                .willRespondWith()
-                .status(200)
-                .body(getFeeResponseBody("FEE0388", 245.00, 1,
-                        "Originating proceedings where no other fee is specified"))
-                .toPact();
+            .uponReceiving("received request for Defendant issue fee")
+            .path(BASEURI)
+            .encodedQuery(defendantFee)
+            .method("GET")
+            .willRespondWith()
+            .status(200)
+            .body(getFeeResponseBody("FEE0388", 245.00, 1,
+                "Originating proceedings where no other fee is specified"))
+            .toPact();
     }
 
     @Test
     @PactTestFor(pactMethod = "getDefendantFee")
     public void verifyDefendantFeePact(MockServer mockServer) throws IOException {
-        HttpResponse httpResponse = Request.Get(mockServer.getUrl() + BASEURI + "?" + DEFENDENT_FEE)
-                .execute().returnResponse();
+        HttpResponse httpResponse = Request.Get(mockServer.getUrl() + BASEURI + "?" + defendantFee)
+            .execute().returnResponse();
     }
 
     @Pact(consumer = "divorce_feeAndPaymentService_feeRegister")
     public RequestResponsePact generalApplicationFee(PactDslWithProvider builder) {
 
         return builder.given("service is registered in Fee registry")
-                .uponReceiving("received request for General Application fee")
-                .path(BASEURI)
-                .encodedQuery(GENRALAPPLICATION_FEE)
-                .method("GET")
-                .willRespondWith()
-                .status(200)
-                .body(getFeeResponseBody("FEE0271", 50.00, 1,
-                        "Application for decree nisi, conditional order, separation order (no fee if undefended"))
-                .toPact();
+            .uponReceiving("received request for General Application fee")
+            .path(BASEURI)
+            .encodedQuery(generalApplicationFee)
+            .method("GET")
+            .willRespondWith()
+            .status(200)
+            .body(getFeeResponseBody("FEE0271", 50.00, 1,
+                "Application for decree nisi, conditional order, separation order (no fee if undefended"))
+            .toPact();
     }
 
     @Test
     @PactTestFor(pactMethod = "generalApplicationFee")
     public void verifyGeneralApplicationFeePact(MockServer mockServer) throws IOException {
-        HttpResponse httpResponse = Request.Get(mockServer.getUrl() + BASEURI + "?" + GENRALAPPLICATION_FEE)
-                .execute().returnResponse();
+        HttpResponse httpResponse = Request.Get(mockServer.getUrl() + BASEURI + "?" + generalApplicationFee)
+            .execute().returnResponse();
     }
 
     @Pact(consumer = "divorce_feeAndPaymentService_feeRegister")
     public RequestResponsePact enforcementFee(PactDslWithProvider builder) {
 
         return builder.given("service is registered in Fee registry")
-                .uponReceiving("received request for Enforcement Application fee")
-                .path(BASEURI)
-                .encodedQuery(ENFORCEMENT_FEE)
-                .method("GET")
-                .willRespondWith()
-                .status(200)
-                .body(getFeeResponseBody("FEE0392", 45.00, 2,
-                        "Request for service by a bailiff of document (see order for exceptions)"))
-                .toPact();
+            .uponReceiving("received request for Enforcement Application fee")
+            .path(BASEURI)
+            .encodedQuery(enforcementFee)
+            .method("GET")
+            .willRespondWith()
+            .status(200)
+            .body(getFeeResponseBody("FEE0392", 45.00, 2,
+                "Request for service by a bailiff of document (see order for exceptions)"))
+            .toPact();
     }
 
 
     @Test
     @PactTestFor(pactMethod = "enforcementFee")
     public void verifyEnforcementFeePact(MockServer mockServer) throws IOException {
-        HttpResponse httpResponse = Request.Get(mockServer.getUrl() + BASEURI + "?" + ENFORCEMENT_FEE)
-                .execute().returnResponse();
+        HttpResponse httpResponse = Request.Get(mockServer.getUrl() + BASEURI + "?" + enforcementFee)
+            .execute().returnResponse();
     }
 
     @Pact(consumer = "divorce_feeAndPaymentService_feeRegister")
     public RequestResponsePact financialOrderFee(PactDslWithProvider builder) {
 
         return builder.given("service is registered in Fee registry")
-                .uponReceiving("received request for Financial Order Application fee")
-                .path(BASEURI)
-                .encodedQuery(FINANCIALORDER_FEE)
-                .method("GET")
-                .willRespondWith()
-                .status(200)
-                .body(getFeeResponseBody("FEE0229", 255.00, 1,
-                        "Application for a financial orde"))
-                .toPact();
+            .uponReceiving("received request for Financial Order Application fee")
+            .path(BASEURI)
+            .encodedQuery(financialOrderFee)
+            .method("GET")
+            .willRespondWith()
+            .status(200)
+            .body(getFeeResponseBody("FEE0229", 255.00, 1,
+                "Application for a financial orde"))
+            .toPact();
     }
 
     @Test
     @PactTestFor(pactMethod = "financialOrderFee")
     public void verifyFinancialOrderFeePact(MockServer mockServer) throws IOException {
-        HttpResponse httpResponse = Request.Get(mockServer.getUrl() + BASEURI + "?" + FINANCIALORDER_FEE)
-                .execute().returnResponse();
+        HttpResponse httpResponse = Request.Get(mockServer.getUrl() + BASEURI + "?" + financialOrderFee)
+            .execute().returnResponse();
     }
 
     @Pact(consumer = "divorce_feeAndPaymentService_feeRegister")
     public RequestResponsePact applicationNoNotice(PactDslWithProvider builder) {
 
         return builder.given("service is registered in Fee registry")
-                .uponReceiving("received request for application no notice fee")
-                .path(BASEURI)
-                .encodedQuery(APPLICATION_NO_NOTICE)
-                .method("GET")
-                .willRespondWith()
-                .status(200)
-                .body(getFeeResponseBody("FEE0228", 50.00, 1,
-                        "Application (without notice)"))
-                .toPact();
+            .uponReceiving("received request for application no notice fee")
+            .path(BASEURI)
+            .encodedQuery(applicationNoNotice)
+            .method("GET")
+            .willRespondWith()
+            .status(200)
+            .body(getFeeResponseBody("FEE0228", 50.00, 1,
+                "Application (without notice)"))
+            .toPact();
     }
 
     @Test
     @PactTestFor(pactMethod = "applicationNoNotice")
     public void verifyApplicationNoNoticeFeePact(MockServer mockServer) throws IOException {
-        HttpResponse httpResponse = Request.Get(mockServer.getUrl() + BASEURI + "?" + APPLICATION_NO_NOTICE)
-                .execute().returnResponse();
+        HttpResponse httpResponse = Request.Get(mockServer.getUrl() + BASEURI + "?" + applicationNoNotice)
+            .execute().returnResponse();
     }
 
     private PactDslJsonBody getFeeResponseBody(String code, double amount, int version, String description) {
         return new PactDslJsonBody()
-                .stringType("code", code)
-                .decimalType("fee_amount", amount)
-                .numberType("version", version)
-                .stringType("description", description);
+            .stringType("code", code)
+            .decimalType("fee_amount", amount)
+            .numberType("version", version)
+            .stringType("description", description);
     }
 }
 
--- src/main/java/uk/gov/hmcts/reform/divorce/feepayment/service/impl/FeePaymentServiceImpl.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/feepayment/service/impl/FeePaymentServiceImpl.java
@@ -24,36 +24,43 @@ public class FeePaymentServiceImpl implements FeePaymentService {
     private static final String VERSION = "version";
     private static final String CODE = "code";
     private static final String DESCRIPTION = "description";
-    private static final String OTHER = "other";
-    private static final String DIVORCE = "divorce";
-    private static final String FINANCIAL_ORDER = "financial-order";
-    private static final String HIJ = "HIJ";
-    private static final String ISSUE = "issue";
-    private static final String GENERAL_APPLICATION = "general application";
 
-    private String genAppWithoutNoticeFeeKeyword;
+    private static final String SERVICE_OTHER = "other";
+    private static final String SERVICE_DIVORCE = "divorce";
+
+    private static final String EVENT_ISSUE = "issue";
+    private static final String EVENT_GENERAL_APPLICATION = "general application";
+    private static final String EVENT_ENFORCEMENT = "enforcement";
+    private static final String EVENT_MISCELLANEOUS = "miscellaneous";
+
+    private static final String KEYWORD_DIVORCE_APPLICATION = "DivorceCivPart";
+    private static final String KEYWORD_AMEND_PETITION = "DivorceAmendPetition";
+    private static final String KEYWORD_ORIGINATE_PROCEEDINGS = "AppnPrivateOther";
+    private static final String KEYWORD_DECREE_NISI = "GAContestedOrder";
+    private static final String KEYWORD_BAILIFF_SERVICE = "BailiffServeDoc";
+    private static final String KEYWORD_FINANCIAL_ORDER = "FinancialOrderOnNotice";
+    private static final String KEYWORD_WITHOUT_NOTICE = "GeneralAppWithoutNotice";
+    protected static final String KEYWORD_ORIGINAL_AMEND = "ABC";
+    protected static final String KEYWORD_ORIGINAL_DEFEND = "PQR";
+    protected static final String KEYWORD_ORIGINAL_BAILIFF = "HIJ";
+    protected static final String KEYWORD_ORIGINAL_FO = "financial-order";
+
     private final String feesLookupEndpoint;
+    private Boolean feesPayKeywords;
     private final RestTemplate restTemplate;
-    private String feeApiBaseUri;
-    private final String[][] feesItems = {
-        {ISSUE, DIVORCE, null},
-        {ISSUE, OTHER, "ABC"},
-        {GENERAL_APPLICATION, OTHER, null},
-        {"enforcement", OTHER, HIJ},
-        {"miscellaneous", OTHER, FINANCIAL_ORDER},
-        {GENERAL_APPLICATION, OTHER, genAppWithoutNoticeFeeKeyword},
-        {ISSUE, OTHER, "PQR"}
-    };
+    private final String feeApiBaseUri;
+
+
 
     @Autowired
     public FeePaymentServiceImpl(RestTemplate restTemplate,
         @Value("${fee.api.baseUri}") String feeApiBaseUri,
         @Value("${fee.api.feesLookup}") String feesLookupEndpoint,
-        @Value("${fee.api.genAppWithoutNoticeFeeKeyword}") String genAppWithoutNoticeFeeKeyword) {
+        @Value("${feature-toggle.toggle.fee-pay-keywords}") Boolean feesPayKeywords) {
         this.restTemplate = restTemplate;
         this.feeApiBaseUri = feeApiBaseUri;
         this.feesLookupEndpoint = feesLookupEndpoint;
-        this.genAppWithoutNoticeFeeKeyword = genAppWithoutNoticeFeeKeyword;
+        this.feesPayKeywords = feesPayKeywords;
     }
 
     @Override
@@ -64,37 +71,37 @@ public class FeePaymentServiceImpl implements FeePaymentService {
 
     @Override
     public Fee getIssueFee() {
-        return getFee(ISSUE, DIVORCE, null );
+        return getFee(EVENT_ISSUE, SERVICE_DIVORCE, (feesPayKeywords ? KEYWORD_DIVORCE_APPLICATION : null));
     }
 
     @Override
     public Fee getAmendPetitionFee() {
-        return getFee(ISSUE, OTHER, "ABC" );
+        return getFee(EVENT_ISSUE, SERVICE_OTHER, (feesPayKeywords ? KEYWORD_AMEND_PETITION : KEYWORD_ORIGINAL_AMEND));
     }
 
     @Override
     public Fee getDefendPetitionFee() {
-        return getFee(ISSUE, OTHER, "PQR" );
+        return getFee(EVENT_ISSUE, SERVICE_OTHER, (feesPayKeywords ? KEYWORD_ORIGINATE_PROCEEDINGS : KEYWORD_ORIGINAL_DEFEND));
     }
 
     @Override
     public Fee getGeneralApplicationFee() {
-        return getFee(GENERAL_APPLICATION, OTHER, null);
+        return getFee(EVENT_GENERAL_APPLICATION, SERVICE_OTHER, (feesPayKeywords ? KEYWORD_DECREE_NISI : null));
     }
 
     @Override
     public Fee getEnforcementFee() {
-        return getFee("enforcement", OTHER, HIJ);
+        return getFee(EVENT_ENFORCEMENT, SERVICE_OTHER, (feesPayKeywords ? KEYWORD_BAILIFF_SERVICE : KEYWORD_ORIGINAL_BAILIFF));
     }
 
     @Override
     public Fee getApplicationFinancialOrderFee() {
-        return getFee("miscellaneous", OTHER, FINANCIAL_ORDER);
+        return getFee(EVENT_MISCELLANEOUS, SERVICE_OTHER, (feesPayKeywords ? KEYWORD_FINANCIAL_ORDER : KEYWORD_ORIGINAL_FO));
     }
 
     @Override
     public Fee getApplicationWithoutNoticeFee() {
-        return getFee(GENERAL_APPLICATION, OTHER, genAppWithoutNoticeFeeKeyword);
+        return getFee(EVENT_GENERAL_APPLICATION, SERVICE_OTHER, KEYWORD_WITHOUT_NOTICE);
     }
 
     private Fee getFromRegister(URI uri) {
@@ -141,7 +148,15 @@ public class FeePaymentServiceImpl implements FeePaymentService {
 
     @Override
     public List<Fee> getAllFees() {
-
+        String[][] feesItems = {
+            {EVENT_ISSUE, SERVICE_DIVORCE, (feesPayKeywords ? KEYWORD_DIVORCE_APPLICATION : null)},
+            {EVENT_ISSUE, SERVICE_OTHER, (feesPayKeywords ? KEYWORD_AMEND_PETITION : KEYWORD_ORIGINAL_AMEND)},
+            {EVENT_GENERAL_APPLICATION, SERVICE_OTHER, (feesPayKeywords ? KEYWORD_DECREE_NISI : null)},
+            {EVENT_ENFORCEMENT, SERVICE_OTHER, (feesPayKeywords ? KEYWORD_BAILIFF_SERVICE : KEYWORD_ORIGINAL_BAILIFF)},
+            {EVENT_MISCELLANEOUS, SERVICE_OTHER, (feesPayKeywords ? KEYWORD_FINANCIAL_ORDER : KEYWORD_ORIGINAL_FO)},
+            {EVENT_GENERAL_APPLICATION, SERVICE_OTHER, KEYWORD_WITHOUT_NOTICE},
+            {EVENT_ISSUE, SERVICE_OTHER, (feesPayKeywords ? KEYWORD_ORIGINATE_PROCEEDINGS : KEYWORD_ORIGINAL_DEFEND)}
+        };
         return Stream.of(feesItems).map(i -> getFee(i[0], i[1], i[2])).collect(Collectors.toList());
     }
 }
