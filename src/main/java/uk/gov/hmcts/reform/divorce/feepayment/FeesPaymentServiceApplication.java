4886132777c25da01225ae4bf870bd5c27661c5c
srochani <sunil.rochani@hotmail.com>
2023-09-26 14:06:25 +0100

    DFR-2321: Upgrade Java17 for div-fees-and-payments-service

--- src/main/java/uk/gov/hmcts/reform/divorce/feepayment/FeesPaymentServiceApplication.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/feepayment/FeesPaymentServiceApplication.java
@@ -3,11 +3,9 @@ package uk.gov.hmcts.reform.divorce.feepayment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import uk.gov.hmcts.reform.authorisation.healthcheck.ServiceAuthHealthIndicator;
 
 @SpringBootApplication(scanBasePackages = {"uk.gov.hmcts.reform.divorce",
-        "uk.gov.hmcts.reform.logging.appinsights" } ,
-        exclude = {ServiceAuthHealthIndicator.class})
+        "uk.gov.hmcts.reform.logging.appinsights" })
 @EnableConfigurationProperties
 public class FeesPaymentServiceApplication {
 
