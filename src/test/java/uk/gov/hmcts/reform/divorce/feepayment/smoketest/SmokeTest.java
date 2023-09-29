b4a17395866d9e71c0f111227bcd9f03bab8e7b7
srochani <sunil.rochani@hotmail.com>
2023-09-29 10:56:34 +0100

    DFR-2321: Upgrade Java17 for div-fees-and-payments-service

--- src/test/java/uk/gov/hmcts/reform/divorce/feepayment/smoketest/SmokeTest.java
+++ src/test/java/uk/gov/hmcts/reform/divorce/feepayment/smoketest/SmokeTest.java
@@ -1,16 +1,16 @@
 package uk.gov.hmcts.reform.divorce.feepayment.smoketest;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import uk.gov.hmcts.reform.divorce.feepayment.controller.FeePaymentServiceController;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class SmokeTest {
 
